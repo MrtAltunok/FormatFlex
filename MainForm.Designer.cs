150fc007cab352b4831af0ec98e3c245f5a8a4e2
Murat <m.altunok@hotmail.com>
2023-08-01 16:35:34 +0300

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace ImageConvert
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnSelectImages = new System.Windows.Forms.Button();
+            this.txtSuffix = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lbFormats = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSelectImages
+            // 
+            this.btnSelectImages.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSelectImages.Location = new System.Drawing.Point(12, 12);
+            this.btnSelectImages.Name = "btnSelectImages";
+            this.btnSelectImages.Size = new System.Drawing.Size(143, 48);
+            this.btnSelectImages.TabIndex = 0;
+            this.btnSelectImages.Text = "Görüntü(leri) Seç";
+            this.btnSelectImages.UseVisualStyleBackColor = true;
+            this.btnSelectImages.Click += new System.EventHandler(this.btnSelectImages_Click);
+            // 
+            // txtSuffix
+            // 
+            this.txtSuffix.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSuffix.Location = new System.Drawing.Point(272, 33);
+            this.txtSuffix.Name = "txtSuffix";
+            this.txtSuffix.Size = new System.Drawing.Size(256, 26);
+            this.txtSuffix.TabIndex = 2;
+            this.txtSuffix.Text = "name";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(542, 12);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 90);
+            this.btnConvert.TabIndex = 3;
+            this.btnConvert.Text = "BAŞLAT";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 63);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 4;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 79);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(520, 23);
+            this.progressBar.TabIndex = 5;
+            // 
+            // lbFormats
+            // 
+            this.lbFormats.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbFormats.FormattingEnabled = true;
+            this.lbFormats.Items.AddRange(new object[] {
+            "JPG",
+            "BMP",
+            "PNG",
+            "WEBP"});
+            this.lbFormats.Location = new System.Drawing.Point(161, 34);
+            this.lbFormats.Name = "lbFormats";
+            this.lbFormats.Size = new System.Drawing.Size(96, 26);
+            this.lbFormats.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(161, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Formatı seçiniz";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(273, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(255, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Yeni oluşacak görüntü ismine varsa eklenecek metin";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 111);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbFormats);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtSuffix);
+            this.Controls.Add(this.btnSelectImages);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormatFlex v.1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectImages;
+        private System.Windows.Forms.TextBox txtSuffix;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.ComboBox lbFormats;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,96 @@
+﻿using ImageMagick;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageConvert
+{
+    public partial class MainForm : Form
+    {
+        private string[] files;
+
+        public MainForm()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void btnSelectImages_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Multiselect = true,
+                Filter = "Images (*.BMP;*.JPG;*.JPEG;*.PNG;*.WEBP)|*.BMP;*.JPG;*.PNG;*.WEBP"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                files = openFileDialog.FileNames;
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (files != null && files.Length > 0 && lbFormats.SelectedItem != null)
+            {
+                lblStatus.Text = "";
+                string format = lbFormats.SelectedItem.ToString();
+                string newFolderPath = Path.GetDirectoryName(files[0]) + "\\yeniFormat";
+
+                if (!Directory.Exists(newFolderPath))
+                {
+                    Directory.CreateDirectory(newFolderPath);
+                }
+
+                int count = 0;
+                foreach (string file in files)
+                {
+                    //string newFileName = Path.GetFileNameWithoutExtension(file) + "_" + txtSuffix.Text + "_" + new Random().Next(100, 9999).ToString();
+                    string newFileName = txtSuffix.Text + "_" + new Random().Next(100, 9999).ToString();
+                    string newFilePath = newFolderPath + "\\" + newFileName + "." + format.ToLower();
+
+                    MagickReadSettings settings = new MagickReadSettings();
+                    using (MagickImage image = new MagickImage(file, settings))
+                    {
+                        image.Write(newFilePath, GetMagickFormat(format));
+                        count++;
+                        progressBar.Value = (int)(((double)count / files.Length) * 100);
+                    }
+                }
+
+                lblStatus.Text = "Dönüştürme işlemi başarılı bir şekilde tamamlandı!";
+                System.Diagnostics.Process.Start("explorer.exe", newFolderPath); // New folder is opened in file explorer
+
+            }
+            else
+            {
+                MessageBox.Show("Lütfen dönüştürülecek dosya veya dosya formatını seçiniz!!");
+            }
+        }
+        private MagickFormat GetMagickFormat(string format)
+        {
+            switch (format.ToLower())
+            {
+                case "jpg":
+                    return MagickFormat.Jpeg;
+                case "jpeg":
+                    return MagickFormat.Jpeg;
+                case "bmp":
+                    return MagickFormat.Bmp;
+                case "png":
+                    return MagickFormat.Png;
+                case "webp":
+                    return MagickFormat.WebP;
+                default:
+                    throw new NotSupportedException("Belirtilen biçim desteklenmiyor!!");
+            }
+        }
+    }
+}
